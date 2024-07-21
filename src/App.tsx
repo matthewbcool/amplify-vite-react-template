d853bd3650fa6bf9da646a254a3c777980e93a7a
Matt <matthewbcool@gmail.com>
2024-07-21 14:10:17 -0700

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,6 @@ import type { Schema } from '../amplify/data/resource';
 import { generateClient } from 'aws-amplify/data';
 import { Test } from './Test.jsx';
 
-const client = generateClient<Schema>();
-
-function deleteTodo(id: string) {
-  client.models.Todo.delete({ id });
-}
-
 function App() {
   return (
     <main>
